d3950d4688b412500d3878135aca3196ec364fd8
huynhvantam <huynhvantamm@gmail.com>
2019-07-23 18:10:09 +0700

    bai tap sudungtoantu

---
+++ ngay3/sudungtoantu/filejs.js
@@ -0,0 +1,20 @@
+let inputWidth;
+let inputHeight;
+
+
+
+inputHeight = prompt("nhap vao height");
+inputWidth = prompt("nhap vao Width");
+
+
+
+
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+
+
+let area = width * height;
+document.write("the area:"+area)
+
+
+
